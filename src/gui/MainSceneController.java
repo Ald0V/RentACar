a20d0e381c73fa722e5510b5bd01ac6de4e1c67b
Aldo <aldoernestocabrera2002@gmail.com>
2023-11-05 10:27:07 -0500

    Boton de salida habilitado

--- src/gui/MainSceneController.java
+++ src/gui/MainSceneController.java
@@ -1,5 +1,20 @@
 package gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
 public class MainSceneController {
+	
+	@FXML
+	private AnchorPane mainScenePane;
+	
+	Stage stage;
+	
+	public void logout(ActionEvent event) {
+		stage = (Stage)mainScenePane.getScene().getWindow(); 
+		stage.close();
+	}
 
 }
